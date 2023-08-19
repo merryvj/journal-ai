8d524d457ebf69a92022621506a7667f49c1d115
Mary Jiang <mvjiang@gmail.com>
2023-08-19 18:24:01 -0400

    routes and api for creating entry

--- components/EntryCard.tsx
+++ components/EntryCard.tsx
@@ -1,13 +1,6 @@
-"use client"
-
 const EntryCard = ({ entry }) => {
     return (
-        <div
-            className="cursor-pointer overflow-hidden rounded-l bg-white shadow"
-        >
-            <div className="px-4 py-5 sm:p-6">
-                
-            </div>
+        <div>
 
         </div>
     )
