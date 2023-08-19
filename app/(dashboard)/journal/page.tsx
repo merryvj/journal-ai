27ecef8f1ddba6a057d78126519458f24889d142
Mary Jiang <mvjiang@gmail.com>
2023-08-19 18:07:33 -0400

    basic new entry card

--- app/(dashboard)/journal/page.tsx
+++ app/(dashboard)/journal/page.tsx
@@ -1,6 +1,35 @@
-const JournalPage = () => {
+import NewEntryCard from "@/components/NewEntryCard"
+import EntryCard from "@/components/EntryCard"
+import { getUserByClerkId } from "@/utils/auth"
+import { prisma } from "@/utils/db"
+
+const getEntries = async () => {
+    const user = getUserByClerkId()
+    const entries = await prisma.journalEntry.findMany({
+        where: {
+            userId: user.id,
+        },
+        orderBy: {
+            createdAt: "desc",
+        }
+    })
+
+    return entries
+}
+
+const JournalPage = async () => {
+    const entries = await getEntries()
+    
     return (
-        <div>Journal</div>
+        <div className="h-full p-10 bg-slate-400/10">
+            <h2 className="text-3xl mb-8">Journal</h2>
+            <div className="grid grid-cols-3 gap-4 p-10">
+            <NewEntryCard/>
+            {entries.map((entry) => (
+                <EntryCard key={entry.id} entry={entry}/>
+            ))}
+        </div>
+        </div>
     )
 }
 
---
+++ components/EntryCard.tsx
@@ -0,0 +1,16 @@
+"use client"
+
+const EntryCard = ({ entry }) => {
+    return (
+        <div
+            className="cursor-pointer overflow-hidden rounded-l bg-white shadow"
+        >
+            <div className="px-4 py-5 sm:p-6">
+                
+            </div>
+
+        </div>
+    )
+}
+
+export default EntryCard
