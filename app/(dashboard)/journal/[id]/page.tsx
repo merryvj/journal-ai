fe8b2598044f4b87e0cb48578d6f9972a1ff003e
Mary Jiang <mvjiang@gmail.com>
2023-08-21 20:26:36 -0400

    getEntry for a page

--- app/(dashboard)/journal/[id]/page.tsx
+++ app/(dashboard)/journal/[id]/page.tsx
@@ -1,6 +1,25 @@
-const EntryPage = ({params}) => {
+import Editor from "@/components/Editor"
+import { prisma } from "@/utils/db"
+import { getUserByClerkId } from "@/utils/auth"
+
+const getEntry = async (id) => {
+    const user = await getUserByClerkId()
+    const entry = await prisma.journalEntry.findUnique({
+        where: {
+            userId: user.id,
+            id
+        }
+    })
+
+    return entry
+}
+
+const EntryPage = async ({params}) => {
+    const entry = await getEntry(params.id)
     return (
-        <div> Page </div>
+        <div>
+            <Editor entry={entry}/>
+        </div>
     )
 }
 
