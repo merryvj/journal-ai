977982d42db1933e08da3c46f1362c973868fb22
Mary Jiang <mvjiang@gmail.com>
2023-08-21 20:06:32 -0400

    basic styled entry card component

--- app/(dashboard)/journal/page.tsx
+++ app/(dashboard)/journal/page.tsx
@@ -2,31 +2,39 @@ import NewEntryCard from "@/components/NewEntryCard"
 import EntryCard from "@/components/EntryCard"
 import { getUserByClerkId } from "@/utils/auth"
 import { prisma } from "@/utils/db"
+import Link from "next/link"
 
 const getEntries = async () => {
-    const user = getUserByClerkId()
-    const entries = await prisma.journalEntry.findMany({
-        where: {
-            userId: user.id,
-        },
-        orderBy: {
-            createdAt: "desc",
-        }
+    const user = await getUserByClerkId()
+    const data = await prisma.journalEntry.findMany({
+      where: {
+        userId: user.id,
+      },
+      orderBy: {
+        createdAt: 'desc',
+      },
+      include: {
+        analysis: true,
+      },
     })
-
-    return entries
-}
+    
+    
+    return data
+  }
 
 const JournalPage = async () => {
     const entries = await getEntries()
-    
+
+
     return (
         <div className="h-full p-10 bg-slate-400/10">
             <h2 className="text-3xl mb-8">Journal</h2>
             <div className="grid grid-cols-3 gap-4 p-10">
             <NewEntryCard/>
             {entries.map((entry) => (
-                <EntryCard key={entry.id} entry={entry}/>
+                <Link href={`/journal/${entry.id}`} key={entry.id}>
+                    <EntryCard entry={entry}/>
+                </Link>
             ))}
         </div>
         </div>
--- components/EntryCard.tsx
+++ components/EntryCard.tsx
@@ -1,7 +1,18 @@
 const EntryCard = ({ entry }) => {
+    const date = new Date(entry.createdAt).toDateString()
     return (
         <div>
-
+            <div className="divide-y">
+                <div className="px-4 py-6 sm:px-6"> 
+                 {date}
+                </div>
+                <div className="px-4 py-5 sm:p-6"> 
+                    summary
+                </div>
+                <div className="px-4 py-4 sm:px-6"> 
+                    mood
+                </div>
+            </div>
         </div>
     )
 }
